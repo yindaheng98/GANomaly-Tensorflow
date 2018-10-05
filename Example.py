e88cbe0443e92b8f4dd6534b7fcf55b869cb8b7c
Howard Yin <770275298@qq.com>
2018-10-05 23:54:13 +0800

    Built an Example.

--- Example.py
+++ Example.py
@@ -1,9 +1,33 @@
 import tensorflow as tf
-from GANomaly.Coders import Encoder,Decoder
+from GANomaly.Coders import Encoder, Decoder
 
 input_shape = 1024
+n_hidden_channel = 16
 x = tf.placeholder(tf.float32, [None, input_shape, input_shape, 3])
-encode = Encoder(x, 16)
-print(encode.get_shape())
-decode = Decoder(encode, 16, 3)
-print(decode.get_shape())
+with tf.variable_scope('encoder1'):
+    z = Encoder(x, n_hidden_channel)
+with tf.variable_scope('decoder1'):
+    x_ = Decoder(z, n_hidden_channel, 3)
+with tf.variable_scope('encoder2'):
+    z_ = Encoder(x_, n_hidden_channel)
+with tf.variable_scope('encoder3'):
+    fx = Encoder(x, n_hidden_channel)
+with tf.variable_scope('encoder3') as scope:
+    scope.reuse_variables()
+    fx_ = Encoder(x_, n_hidden_channel)
+predict = tf.nn.sigmoid(fx_)
+
+loss_con = tf.reduce_mean(tf.abs(x - x_))
+loss_enc = tf.reduce_mean((z - z_) ** 2)
+loss_adv = tf.reduce_mean(fx - fx_)
+
+print(x.get_shape())
+print(z.get_shape())
+print(x_.get_shape())
+print(z_.get_shape())
+print(fx.get_shape())
+print(fx_.get_shape())
+print(predict.get_shape())
+print(loss_con.get_shape())
+print(loss_enc.get_shape())
+print(loss_adv.get_shape())
--- GANomaly/Coders.py
+++ GANomaly/Coders.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 
 
-def Encoder(x, n_hidden_channel, n_extra_layer=0):
+def Encoder(x, n_hidden_channel=16, n_extra_layer=0):
     """
     编码器
     :param x: 输入
@@ -10,54 +10,61 @@ def Encoder(x, n_hidden_channel, n_extra_layer=0):
     :param n_extra_layer: 额外层层数
     :return: 输出
     """
-    shape = x.get_shape().as_list()[1]
+    shape = x.get_shape().as_list()
+    channel = x.get_shape().as_list()[3]
+    shape = shape[1]
     assert x.get_shape().as_list()[2] == shape, '输入网络中的图像必须是正方形'
     assert (shape & shape - 1) == 0, '输入网络中的图像的边长必须是2的整数次幂'
     assert (n_hidden_channel & n_hidden_channel - 1) == 0, '隐藏层通道数必须是2的整数次幂'
 
-    with tf.name_scope('encode_initial_layer'):  #初始层，通道数增加到n_hidden_channel，边长减半
-        conv = tf.layers.conv2d(x, filters=n_hidden_channel, kernel_size=4, strides=2, padding='same')
-        relu = tf.nn.leaky_relu(conv, 0.2)
-        last_layer = relu
-        shape /= 2
+    #初始层，通道数增加到n_hidden_channel，边长减半
+    w_conv = tf.get_variable('encoder_wconv_initial', shape=[4, 4, channel, n_hidden_channel],
+                             initializer=tf.truncated_normal_initializer(stddev=0.1))
+    conv = tf.nn.conv2d(x, filter=w_conv, strides=[1, 2, 2, 1], padding='SAME')
+    relu = tf.nn.leaky_relu(conv, 0.2)
+    last_layer = relu
+    shape /= 2
 
+    #额外层，通道数不变，大小不变
     for i in range(1, n_extra_layer + 1):
-        with tf.name_scope('encode_extra_layer_%d' % i):  #额外层，通道数不变，大小不变
-            conv = tf.layers.conv2d(last_layer, filters=n_hidden_channel, kernel_size=3, strides=1, padding='same')
-            batch_norm = tf.layers.batch_normalization(conv)
-            relu = tf.nn.leaky_relu(batch_norm, 0.2)
-            last_layer = relu
+        w_conv = tf.get_variable('encoder_extra_wconv_%d' % i, shape=[3, 3, n_hidden_channel, n_hidden_channel],
+                                 initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv = tf.nn.conv2d(last_layer, filter=w_conv, strides=[1, 1, 1, 1], padding='SAME')
+        batch_norm = tf.layers.batch_normalization(conv)
+        relu = tf.nn.leaky_relu(batch_norm, 0.2)
+        last_layer = relu
 
+    #处理层，一步步把数据压缩成channel*4*4
+    i_layer = 0  #记录层数
     channel = n_hidden_channel  #记录通道数，每层通道数倍增
-    i_layer = 0  #记录层数，每层加一
-    while shape > 4:  #处理层，一步步把数据压缩成channel*4*4
+    while shape > 4:
         i_layer += 1
-        with tf.name_scope('encode_layer_%d' % i_layer):
-            channel *= 2
-            conv = tf.layers.conv2d(last_layer, channel, kernel_size=4, strides=2, padding='same')
-            batch_norm = tf.layers.batch_normalization(conv)
-            relu = tf.nn.leaky_relu(batch_norm, 0.2)
-            last_layer = relu
-            shape /= 2  #每层长宽减半
+        channel *= 2
+        w_conv = tf.get_variable('encoder_wconv_%d' % i_layer, shape=[4, 4, channel // 2, channel],
+                                 initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv = tf.nn.conv2d(last_layer, filter=w_conv, strides=[1, 2, 2, 1], padding='SAME')
+        batch_norm = tf.layers.batch_normalization(conv)
+        relu = tf.nn.leaky_relu(batch_norm, 0.2)
+        last_layer = relu
+        shape /= 2  #每层长宽减半
 
     #最后一层把数据压缩到channel*1*1
-    conv = tf.layers.conv2d(last_layer,
-                            filters=channel,
-                            kernel_size=4, strides=1,
-                            padding='valid',
-                            name="encode_output_layer")
+    w_conv = tf.get_variable('encoder_w_conv_final', shape=[4, 4, channel, channel],
+                             initializer=tf.truncated_normal_initializer(stddev=0.1))
+    conv = tf.nn.conv2d(last_layer, filter=w_conv, strides=[1, 1, 1, 1], padding='VALID')
+
     last_layer = conv
     return last_layer
 
 
 
-def Decoder(x, n_hidden_channel, n_output_channel, n_extra_layer=0):
+def Decoder(x, n_hidden_channel=16, n_output_channel=3, n_extra_layer=0):
     """
     解码器
     :param x: 输入
     :param n_hidden_channel: 隐藏层通道数
+    :param n_output_channel: 输出通道数
     :param n_extra_layer: 额外层层数
-    :param output_channel: 输出通道数
     :return: 输出
     """
     xshape = x.get_shape().as_list()
@@ -65,37 +72,46 @@ def Decoder(x, n_hidden_channel, n_output_channel, n_extra_layer=0):
     assert (xshape[3] & xshape[3] - 1) == 0, '输入向量长度必须是2的整数次幂'
     assert (n_hidden_channel & n_hidden_channel - 1) == 0, '隐藏层通道数必须是2的整数次幂'
 
+    shape = 4  #为tf.nn.conv2d_transpose中那个无用的参数output_shape记录图像边长
     channel = xshape[3]
-    with tf.name_scope('decode_initial_layer'):
+    w_conv = tf.get_variable('decoder_wconv_initial', shape=[4, 4, channel, channel],
+                             initializer=tf.truncated_normal_initializer(stddev=0.1))
+    conv = tf.nn.conv2d_transpose(x, filter=w_conv, strides=[1, 1, 1, 1], padding='SAME',
+                                  output_shape=[-1, shape, shape, channel])
+    batch_norm = tf.layers.batch_normalization(conv)
+    relu = tf.nn.relu(batch_norm)
+    last_layer = relu
+
+    #处理层，一步步把数据恢复成n_hidden_channel*shape*shape
+    i_layer = 0  #记录层数
+    while channel > n_hidden_channel:
+        i_layer += 1
+        shape *= 2
         channel //= 2
-        conv_t = tf.layers.conv2d_transpose(x, channel, kernel_size=4, strides=1, padding='valid')
-        batch_norm = tf.layers.batch_normalization(conv_t)
+        w_conv = tf.get_variable('decoder_wconv_%d' % i_layer, shape=[4, 4, channel, channel * 2],
+                                 initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv = tf.nn.conv2d_transpose(last_layer, filter=w_conv, strides=[1, 2, 2, 1], padding='SAME',
+                                      output_shape=[-1, shape, shape, channel])
+        batch_norm = tf.layers.batch_normalization(conv)
         relu = tf.nn.relu(batch_norm)
         last_layer = relu
 
-    i_layer = 0  #记录层数，每层加一
-    while channel >= n_hidden_channel:
-        i_layer += 1
-        with tf.name_scope('decode_layer_%d' % i_layer):  #处理层，一步步把数据恢复成n_hidden_channel*shape*shape
-            channel //= 2
-            conv_t = tf.layers.conv2d_transpose(last_layer, channel, kernel_size=4, strides=2, padding='same')
-            batch_norm = tf.layers.batch_normalization(conv_t)
-            relu = tf.nn.relu(batch_norm)
-            last_layer = relu
-
+    #额外层，通道数不变，大小不变
     for i in range(1, n_extra_layer + 1):
-        with tf.name_scope('decode_extra_layer_%d' % i):  #额外层，通道数不变，大小不变
-            conv_t = tf.layers.conv2d_transpose(last_layer, channel, kernel_size=3, strides=1, padding='same')
-            batch_norm = tf.layers.batch_normalization(conv_t)
-            relu = tf.nn.relu(batch_norm)
-            last_layer = relu
+        w_conv = tf.get_variable('decoder_extra_wconv_%d' % i, shape=[3, 3, channel, channel],
+                                 initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv = tf.nn.conv2d_transpose(last_layer, filter=w_conv, strides=[1, 1, 1, 1], padding='SAME',
+                                      output_shape=[-1, shape, shape, channel])
+        batch_norm = tf.layers.batch_normalization(conv)
+        relu = tf.nn.relu(batch_norm)
+        last_layer = relu
 
     #最后一层把数据恢复到channel*1*1
-    conv_t = tf.layers.conv2d_transpose(last_layer,
-                                        filters=n_output_channel,
-                                        kernel_size=4,
-                                        strides=2,
-                                        padding='same',
-                                        name='decode_output_layer')
-    last_layer = conv_t
+    shape *= 2
+    w_conv = tf.get_variable('decoder_w_conv_final', shape=[4, 4, n_output_channel, channel],
+                             initializer=tf.truncated_normal_initializer(stddev=0.1))
+    conv = tf.nn.conv2d_transpose(last_layer, filter=w_conv, strides=[1, 2, 2, 1], padding='SAME',
+                                  output_shape=[-1, shape, shape, n_output_channel])
+    tanh = tf.nn.tanh(conv)
+    last_layer = tanh
     return last_layer
