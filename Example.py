8ba5fada15c9b8aae697b7ea5839d5c665ed0150
Howard Yin <770275298@qq.com>
2018-10-06 20:28:16 +0800

    Examples can run now,maybe.

--- Example.py
+++ Example.py
@@ -1,13 +1,20 @@
 import tensorflow as tf
 from GANomaly.Coders import Encoder, Decoder
+from GANomaly.Movie2TFRecord import dataset_from_movie
 
+#options
 input_shape = 1024
-n_hidden_channel = 16
+n_hidden_channel = 32
+w_loss_con = 1
+w_loss_enc = 1
+w_loss_adv = 1
+
+#model
 x = tf.placeholder(tf.float32, [None, input_shape, input_shape, 3])
 with tf.variable_scope('encoder1'):
     z = Encoder(x, n_hidden_channel)
 with tf.variable_scope('decoder1'):
-    x_ = Decoder(z, n_hidden_channel, 3)
+    x_ = Decoder(z, n_hidden_channel)
 with tf.variable_scope('encoder2'):
     z_ = Encoder(x_, n_hidden_channel)
 with tf.variable_scope('encoder3'):
@@ -17,17 +24,26 @@ with tf.variable_scope('encoder3') as scope:
     fx_ = Encoder(x_, n_hidden_channel)
 predict = tf.nn.sigmoid(fx_)
 
+#lost function
 loss_con = tf.reduce_mean(tf.abs(x - x_))
 loss_enc = tf.reduce_mean((z - z_) ** 2)
 loss_adv = tf.reduce_mean(fx - fx_)
+loss = w_loss_con * loss_con + w_loss_enc * loss_enc + w_loss_adv * loss_adv
+optimizer = tf.train.AdamOptimizer(1.0).minimize(loss)
 
-print(x.get_shape())
-print(z.get_shape())
-print(x_.get_shape())
-print(z_.get_shape())
-print(fx.get_shape())
-print(fx_.get_shape())
-print(predict.get_shape())
-print(loss_con.get_shape())
-print(loss_enc.get_shape())
-print(loss_adv.get_shape())
+#dataset
+dataset = dataset_from_movie('test/33M00S.mp4')
+dataset = dataset.map(lambda e: tf.image.resize_images(e['image'], [input_shape, input_shape], method=1))
+dataset = dataset.repeat()
+dataset = dataset.batch(16)
+iterator = dataset.make_initializable_iterator()
+next_element = iterator.get_next()
+
+#run
+with tf.Session() as sess:
+    sess.run(iterator.initializer)
+    sess.run(tf.global_variables_initializer())
+    for i in range(0, 40):
+        img = sess.run(next_element)
+        l, _ = sess.run([loss, optimizer], feed_dict={x: img})
+        print(l)
--- M2TExample.py
+++ M2TExample.py
@@ -4,14 +4,17 @@ import cv2
 from GANomaly.Coders import Encoder, Decoder
 from GANomaly.Movie2TFRecord import dataset_from_movie
 
+#options
 input_shape = 1024
-n_hidden_channel = 16
-x = tf.placeholder(tf.float32, [None, input_shape, input_shape, 3])
-z = Encoder(x, n_hidden_channel)
-x_ = Decoder(z, n_hidden_channel, 3)
-loss = tf.reduce_mean(tf.abs(x - x_))
-optimiser = tf.train.AdamOptimizer(1.0).minimize(loss)
 
+#model
+x = tf.placeholder(tf.float32, [None, input_shape, input_shape, 3])
+z = Encoder(x)
+x_ = Decoder(z)
+loss = tf.reduce_mean(tf.abs(x - x_))
+optimizer = tf.train.AdamOptimizer(1.0).minimize(loss)
+
+#dataset
 dataset = dataset_from_movie('test/33M00S.mp4')
 dataset = dataset.map(lambda e: tf.image.resize_images(e['image'], [input_shape, input_shape], method=1))
 dataset = dataset.repeat()
@@ -19,13 +22,16 @@ dataset = dataset.batch(1)
 iterator = dataset.make_initializable_iterator()
 next_element = iterator.get_next()
 
+#run
 with tf.Session() as sess:
     sess.run(iterator.initializer)
-    #sess.run(tf.global_variables_initializer())
-    for i in range(1, 1000):
+    sess.run(tf.global_variables_initializer())
+    for i in range(0, 670):
         img = sess.run(next_element)
+        '''
         frame = img[0, :, :, :]
         cv2.imshow('hhh', frame[:, :, 0:3])
         cv2.waitKey(25)
-        #l, _ = sess.run([loss, optimiser], feed_dict={x: img})
-        #print(l)
+        '''
+        l, _ = sess.run([loss, optimizer], feed_dict={x: img})
+        print(l)
